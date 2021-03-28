a8fd265aa9e947f595a85499abd62154055b0f8c
jjjjonathan <hornathanjon@gmail.com>
2021-03-27 23:32:58 -0500

    exercise 1.6 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,22 +1,50 @@
 import React, { useState } from "react";
 
+const Button = ({ type, onClick }) => {
+  return (
+    <button type="button" onClick={onClick}>
+      {type}
+    </button>
+  );
+};
+
+const Status = ({ type, count }) => {
+  return (
+    <p>
+      {type} {count}
+    </p>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
   return (
-    <div>
-      <header>
-        <h1>give feedback</h1>
-      </header>
-      <main>
-        <p>buttons here</p>
-        <h2>statistics</h2>
-        <p>good 6</p>
-      </main>
-    </div>
+    <main>
+      <h1>give feedback</h1>
+      <Button type="good" onClick={handleGoodClick} />
+      <Button type="neutral" onClick={handleNeutralClick} />
+      <Button type="bad" onClick={handleBadClick} />
+      <h2>statistics</h2>
+      <Status type="good" count={good} />
+      <Status type="neutral" count={neutral} />
+      <Status type="bad" count={bad} />
+    </main>
   );
 };
 
