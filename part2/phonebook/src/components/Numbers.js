29690f7b35eec7ce7377fcc35e6e88c7b58f820d
jjjjonathan <hornathanjon@gmail.com>
2021-03-28 18:32:31 -0500

    exercise 2.16 complete

--- part2/phonebook/src/components/Numbers.js
+++ part2/phonebook/src/components/Numbers.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Numbers = ({ persons, query }) => {
+const Numbers = ({ persons, query, onDeleteClick }) => {
   const filteredPersons = query
     ? persons.filter((person) => {
         return person.name.toLowerCase().includes(query.toLowerCase());
@@ -11,7 +11,8 @@ const Numbers = ({ persons, query }) => {
     <ul>
       {filteredPersons.map((person) => (
         <li key={person.name}>
-          {person.name} {person.number}
+          {person.name} {person.number}{" "}
+          <button type="button" onClick={onDeleteClick} />
         </li>
       ))}
     </ul>
