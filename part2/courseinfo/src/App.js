5f24ef739aa151760f690eb9d09986c5be3496dd
jjjjonathan <hornathanjon@gmail.com>
2021-03-28 01:05:47 -0500

    exercise 2.1 complete

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 const Header = ({ course }) => {
   return <h1>{course.name}</h1>;
@@ -24,37 +23,59 @@ const Part = (props) => {
 const Content = ({ course }) => {
   return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      {course.parts.map((part) => {
+        return <Part key={part.id} part={part} />;
+      })}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      {/* <Total course={course} /> */}
     </div>
   );
 };
 
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 5,
+        id: 4,
+      },
+      {
+        name: "React Native",
+        exercises: 27,
+        id: 5,
       },
     ],
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course course={course} />
     </div>
   );
 };
