c3238d6a967455df9e0f09e22383e8d87d51b664
jjjjonathan <hornathanjon@gmail.com>
2021-03-28 01:09:35 -0500

    exercises 2.2 and 2.3 complete

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,11 +5,14 @@ const Header = ({ course }) => {
 };
 
 const Total = ({ course }) => {
-  const sum =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises;
-  return <p>Number of exercises {sum}</p>;
+  const sum = course.parts.reduce((acc, part) => {
+    return acc + part.exercises;
+  }, 0);
+  return (
+    <p>
+      <strong>Total number of exercises {sum}</strong>
+    </p>
+  );
 };
 
 const Part = (props) => {
@@ -35,7 +38,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
-      {/* <Total course={course} /> */}
+      <Total course={course} />
     </div>
   );
 };
