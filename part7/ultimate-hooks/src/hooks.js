092de88d1e97573b2af639c4bcdb7d59f66bf2be
jjjjonathan <hornathanjon@gmail.com>
2021-04-19 01:33:21 -0500

    exercise 7.8 complete

---
+++ part7/ultimate-hooks/src/hooks.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([]);
+
+  const getAll = async () => {
+    const response = await axios.get(baseUrl);
+    setResources(response.data);
+  };
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource);
+    setResources([...resources, response.data]);
+  };
+
+  const service = {
+    getAll,
+    create,
+  };
+
+  return [resources, service];
+};
